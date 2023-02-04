1fb19c4ab7231603caff288d2bae336953e9d490
TadaoYamaoka <tadaoyamaoka@gmail.com>
2023-02-04 16:15:59 +0900

    openvino

--- test_onnx.py
+++ test_onnx.py
@@ -2,17 +2,21 @@ import whisper
 import torch
 import onnxruntime
 import numpy as np
+import time
+
+device = 'cpu'
 
 model = whisper.load_model("base", device='cpu')
 
-audio = whisper.load_audio(r"R:a.wav")
+audio = whisper.load_audio(r"../WhisperTest/a.wav")
 audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
 mel = whisper.log_mel_spectrogram(audio).unsqueeze(0)
 
-encoder_session = onnxruntime.InferenceSession('encoder.onnx', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+encoder_session = onnxruntime.InferenceSession('encoder.onnx', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'] if device == 'cuda' else ['CPUExecutionProvider'])
 
+time_start = time.time()
 encoder_io_binding = encoder_session.io_binding()
 encoder_io_binding.bind_cpu_input('mel', mel.numpy())
 encoder_io_binding.bind_output('n_layer_cross_k', device_type='cuda')
@@ -22,17 +26,21 @@ encoder_session.run_with_iobinding(encoder_io_binding)
 
 n_layer_cross_k, n_layer_cross_v = encoder_io_binding.get_outputs()
 
+print(time.time()- time_start)
+
 print(n_layer_cross_k.shape())
 print(n_layer_cross_v.shape())
 
 tokenizer = whisper.tokenizer.get_tokenizer(model.is_multilingual)
 n_audio = mel.shape[0]
 tokens = torch.tensor([[tokenizer.sot, tokenizer.sot, tokenizer.sot]] * n_audio)  # [n_audio, 3]
-n_layer_self_k_cache = onnxruntime.OrtValue.ortvalue_from_shape_and_type((len(model.decoder.blocks), n_audio, model.dims.n_text_ctx, model.dims.n_text_state), element_type=np.float32, device_type='cuda', device_id=0)
-n_layer_self_v_cache = onnxruntime.OrtValue.ortvalue_from_shape_and_type((len(model.decoder.blocks), n_audio, model.dims.n_text_ctx, model.dims.n_text_state), element_type=np.float32, device_type='cuda', device_id=0)
+n_layer_self_k_cache = onnxruntime.OrtValue.ortvalue_from_shape_and_type((len(model.decoder.blocks), n_audio, model.dims.n_text_ctx, model.dims.n_text_state), element_type=np.float32, device_type=device, device_id=0)
+n_layer_self_v_cache = onnxruntime.OrtValue.ortvalue_from_shape_and_type((len(model.decoder.blocks), n_audio, model.dims.n_text_ctx, model.dims.n_text_state), element_type=np.float32, device_type=device, device_id=0)
 offset = np.zeros(1, dtype=np.int64)
 
-decoder_session = onnxruntime.InferenceSession('decoder.onnx', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+decoder_session = onnxruntime.InferenceSession('decoder.onnx', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'] if device == 'cuda' else ['CPUExecutionProvider'])
+
+time_start = time.time()
 decoder_io_binding = decoder_session.io_binding()
 decoder_io_binding.bind_cpu_input('tokens', tokens.numpy())
 decoder_io_binding.bind_ortvalue_input('in_n_layer_self_k_cache', n_layer_self_k_cache)
@@ -47,6 +55,8 @@ decoder_io_binding.bind_output('out_n_layer_self_v_cache')
 decoder_session.run_with_iobinding(decoder_io_binding)
 
 logits, n_layer_self_k_cache, n_layer_self_v_cache = decoder_io_binding.get_outputs()
+
+print(time.time()- time_start)
 
 print(logits.shape())
 print(n_layer_self_k_cache.shape())
@@ -54,6 +64,7 @@ print(n_layer_self_v_cache.shape())
 
 offset = np.array([tokens.shape[1]], dtype=np.int64)
 tokens = torch.tensor([[tokenizer.sot]] * n_audio)  # [n_audio, 1]
+time_start = time.time()
 decoder_io_binding = decoder_session.io_binding()
 decoder_io_binding.bind_cpu_input('tokens', tokens.numpy())
 decoder_io_binding.bind_ortvalue_input('in_n_layer_self_k_cache', n_layer_self_k_cache)
@@ -68,6 +79,8 @@ decoder_io_binding.bind_output('out_n_layer_self_v_cache')
 decoder_session.run_with_iobinding(decoder_io_binding)
 
 logits, n_layer_self_k_cache, n_layer_self_v_cache = decoder_io_binding.get_outputs()
+
+print(time.time()- time_start)
 
 print(logits.shape())
 print(n_layer_self_k_cache.shape())
---
+++ test_openvino.py
@@ -0,0 +1,71 @@
+import whisper
+from openvino.runtime import Core
+import torch
+import numpy as np
+import time
+
+model = whisper.load_model("base", device='cpu')
+
+audio = whisper.load_audio(r"../WhisperTest/a.wav")
+audio = whisper.pad_or_trim(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).unsqueeze(0)
+
+# Load the network to OpenVINO Runtime.
+ie = Core()
+model_encoder = ie.compile_model(model=ie.read_model(model='encoder.onnx'), device_name="CPU")
+
+output_n_layer_cross_k = model_encoder.output(0)
+output_n_layer_cross_v = model_encoder.output(1)
+
+time_start = time.time()
+output_encoder = model_encoder(mel.numpy())
+n_layer_cross_k = output_encoder[output_n_layer_cross_k]
+n_layer_cross_v = output_encoder[output_n_layer_cross_v]
+
+print(time.time()- time_start)
+
+print(n_layer_cross_k.shape)
+print(n_layer_cross_v.shape)
+
+tokenizer = whisper.tokenizer.get_tokenizer(model.is_multilingual)
+n_audio = mel.shape[0]
+tokens = np.array([[tokenizer.sot, tokenizer.sot, tokenizer.sot]] * n_audio, np.float32)  # [n_audio, 3]
+n_layer_self_k_cache = np.empty((len(model.decoder.blocks), n_audio, model.dims.n_text_ctx, model.dims.n_text_state), np.float32)
+n_layer_self_v_cache = np.empty((len(model.decoder.blocks), n_audio, model.dims.n_text_ctx, model.dims.n_text_state), np.float32)
+offset = np.zeros(1, dtype=np.int64)
+
+model_decoder = ie.compile_model(model=ie.read_model(model='decoder.onnx'), device_name="CPU")
+
+output_logits = model_decoder.output(0)
+output_n_layer_self_k_cache = model_decoder.output(1)
+output_n_layer_self_v_cache = model_decoder.output(2)
+
+time_start = time.time()
+output_decoder = model_decoder([tokens, n_layer_self_k_cache, n_layer_self_v_cache, n_layer_cross_k, n_layer_cross_v, offset])
+
+print(time.time()- time_start)
+
+logits = output_decoder[output_logits]
+n_layer_self_k_cache = output_decoder[output_n_layer_self_k_cache]
+n_layer_self_v_cache = output_decoder[output_n_layer_self_v_cache]
+
+print(logits.shape)
+print(n_layer_self_k_cache.shape)
+print(n_layer_self_v_cache.shape)
+
+offset = np.array([tokens.shape[1]], dtype=np.int64)
+tokens = np.array([[tokenizer.sot]] * n_audio, np.float32)  # [n_audio, 1]
+time_start = time.time()
+output_decoder = model_decoder([tokens, n_layer_self_k_cache, n_layer_self_v_cache, n_layer_cross_k, n_layer_cross_v, offset])
+
+print(time.time()- time_start)
+
+logits = output_decoder[output_logits]
+n_layer_self_k_cache = output_decoder[output_n_layer_self_k_cache]
+n_layer_self_v_cache = output_decoder[output_n_layer_self_v_cache]
+
+print(logits.shape)
+print(n_layer_self_k_cache.shape)
+print(n_layer_self_v_cache.shape)
